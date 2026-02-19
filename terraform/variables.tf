07d56528611d748d237e3ad0b6278ae486e2763c
swathisura <swathisura65@gmail.com>
2026-02-19 12:37:32 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,6 +1,5 @@
 variable "aws_region" {
-  description = "AWS region"
-  default     = "us-east-1"
+  default = "us-east-1"
 }
 
 variable "ecs_cluster_name" {
@@ -15,18 +14,11 @@ variable "ecs_task_family" {
   default = "strapi-task"
 }
 
-variable "instance_type" {
-  default = "t3.micro"
-}
-
 variable "desired_count" {
   default = 1
-}
-
-variable "ecr_repo_name" {
-  default = "strapi-app"
 }
 
 variable "docker_image_tag" {
   default = "latest"
 }
+
