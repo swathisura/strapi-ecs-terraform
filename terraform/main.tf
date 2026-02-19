01826fc5648136d0da543c826042f9d09f405d12
swathisura <swathisura65@gmail.com>
2026-02-19 12:03:11 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,8 +48,10 @@ resource "aws_ecs_cluster" "strapi_cluster" {
 # -------------------------------
 # IAM Role for ECS Task Execution
 # -------------------------------
-resource "aws_iam_role" "ecs_task_execution_role" {
+data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecs-task-execution-role"
+}
+
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -64,7 +66,7 @@ resource "aws_iam_role" "ecs_task_execution_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
+  role       = data.aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
@@ -78,7 +80,7 @@ resource "aws_ecs_task_definition" "strapi_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
